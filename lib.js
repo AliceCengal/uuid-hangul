8469a857ca454148e3fd88fbbe735e6d3f300240
Athran Rahman <cengallut@gmail.com>
2023-03-11 01:00:51 +0800

    improved encoding to 10 characters

--- lib.js
+++ lib.js
@@ -1,7 +1,7 @@
 const HANGUL_START = 44032;
 
 export function encode(uuidv4) {
-  const bitarray = parse12(uuidv4);
+  const bitarray = parse13(uuidv4);
   const output = Array(bitarray.length).fill(1);
   for (const i in bitarray) {
     output[i] = String.fromCodePoint(HANGUL_START + bitarray[i]);
@@ -10,49 +10,48 @@ export function encode(uuidv4) {
 }
 
 export function decode(code) {
-  const bitarray = Array(code.length).fill(0);
+  const bitarray = Array(10).fill("");
   for (const i in code) {
-    bitarray[i] = (code.codePointAt(i) - HANGUL_START).toString(16);
-    if (i % 3 !== 0) {
-      bitarray[i] = bitarray[i].padStart(3, "0");
+    if (i == 0 || i == 9) {
+      bitarray[i] = (code.codePointAt(i) - HANGUL_START).toString(16).padStart(3, '0')
     } else {
-      bitarray[i] = bitarray[i].padStart(2, "0");
+      bitarray[i] = (code.codePointAt(i) - HANGUL_START).toString(2).padStart(13, '0');
     }
   }
 
-  return [
-    bitarray[0] + bitarray[1] + bitarray[2],
-    bitarray[3] + bitarray[4].slice(0, 2),
-    bitarray[4].slice(2) + bitarray[5],
-    bitarray[6] + bitarray[7].slice(0, 2),
-    bitarray[7].slice(2) +
-    bitarray[8] +
-    bitarray[9] +
-    bitarray[10] +
-    bitarray[11],
-    // ];
-  ].join("-");
+  const first = bitarray[0]
+  const second = BigInt("0b" + bitarray.slice(1, 5).join("")).toString(16).padStart(13, '0')
+  const third = BigInt("0b" + bitarray.slice(5, 9).join("")).toString(16).padStart(13, '0')
+  const fourth = bitarray[9]
+
+  const output = [
+    first + second.slice(0, 5),
+    second.slice(5, 9),
+    second.slice(9, 13),
+    third.slice(0, 4),
+    third.slice(4) + fourth,
+  ]
+  return output.join("-")
 }
 
-function parse12(uuid) {
+const mask1 = 0x1fffn;
+
+function parse13(uuid) {
   let v;
-  const arr = Array(12).fill(0);
+  const arr = Array(10).fill(0)
+  arr[0] = parseInt(uuid.slice(0, 3), 16)
 
-  arr[0] = (v = parseInt(uuid.slice(0, 8), 16)) >>> 24;
-  arr[1] = (v >>> 12) & 0xfff;
-  arr[2] = v & 0xfff;
+  arr[1] = Number((v = BigInt("0x" + uuid.slice(3, 8) + uuid.slice(9, 13) + uuid.slice(14, 18))) >> 39n);
+  arr[2] = Number((v >> 26n) & mask1);
+  arr[3] = Number((v >> 13n) & mask1);
+  arr[4] = Number(v & mask1);
 
-  arr[3] = (v = parseInt(uuid.slice(9, 13) + uuid.slice(14, 18), 16)) >>> 24;
-  arr[4] = (v >>> 12) & 0xfff;
-  arr[5] = v & 0xfff;
+  arr[5] = Number((v = BigInt("0x" + uuid.slice(19, 23) + uuid.slice(24, 33), 16)) >> 39n);
+  arr[6] = Number((v >> 26n) & mask1);
+  arr[7] = Number((v >> 13n) & mask1);
+  arr[8] = Number(v & mask1);
 
-  arr[6] = (v = parseInt(uuid.slice(19, 23) + uuid.slice(24, 28), 16)) >>> 24;
-  arr[7] = (v >>> 12) & 0xfff;
-  arr[8] = v & 0xfff;
-
-  arr[9] = (v = parseInt(uuid.slice(28, 36), 16)) >>> 24;
-  arr[10] = (v >>> 12) & 0xfff;
-  arr[11] = v & 0xfff;
+  arr[9] = parseInt(uuid.slice(33, 36), 16)
 
   return arr;
 }
